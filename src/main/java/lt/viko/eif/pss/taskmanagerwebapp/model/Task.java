de4a4e1ca5d4cd2b1b4ff15d1d09f39dfe3959f1
Sindija Tamulytė <96385544+Sind1@users.noreply.github.com>
2023-06-10 15:45:04 +0300

    Changed database variable for status

--- src/main/java/lt/viko/eif/pss/taskmanagerwebapp/model/Task.java
+++ src/main/java/lt/viko/eif/pss/taskmanagerwebapp/model/Task.java
@@ -16,25 +16,29 @@ public class Task {
     @Id
     @GeneratedValue
     private Long id;
+
     @NonNull
     @Column(name = "title")
     private String name;
+
     @Column(length = 2000)
     private String description;
+
     @Column(name = "due_date")
     private LocalDateTime dueDate;
 
     @Column(name = "priority")
     private String priority;
 
-    @Column(name = "status")
-    private String status;
-
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Status status;
+
     @ManyToOne(cascade=CascadeType.ALL)
     private User user;
 }
