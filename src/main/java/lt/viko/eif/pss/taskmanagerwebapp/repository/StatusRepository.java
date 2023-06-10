06306c13eb5b26a40fff56621726f7d6d76497b4
Sindija Tamulytė <96385544+Sind1@users.noreply.github.com>
2023-06-10 15:54:34 +0300

    Updated for status class

--- src/main/java/lt/viko/eif/pss/taskmanagerwebapp/repository/StatusRepository.java
+++ src/main/java/lt/viko/eif/pss/taskmanagerwebapp/repository/StatusRepository.java
@@ -3,6 +3,7 @@ package lt.viko.eif.pss.taskmanagerwebapp.repository;
 import lt.viko.eif.pss.taskmanagerwebapp.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 /**
 
  Repository interface for managing status entities.
